ffeb707dd08486590d60f36e8ae02951276887ae
wangzheng <zheng.wang@fih-foxconn.com>
2020-11-26 11:40:26 +0800

    add create command

--- bin/wing.js
+++ bin/wing.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 
-console.log('welcome to wing with node');
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+'use strict';
+const { satisfies } = require('semver');
+const chalk = require('chalk');
+const { engines, version } = require('../package.json');
+
+function checkNodeVersion(expectedVersion, pakName) {
+  if (!satisfies(process.version, expectedVersion)) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}, but this version of ${pakName} requires Node ${expectedVersion}'.\n\nPlease upgrade your Node version.`
+      )
+    );
+    process.exit(1);
+  }
+}
+checkNodeVersion(engines.node, 'wing-cli');
+
+const program = require('commander');
+const createAction = require('../lib/action/create');
+
+program.version(version).usage('<command> [options]');
+
+// Create Command
+program.command('create <app-name>').action((appName) => {
+  createAction(appName);
+});
+
+// init Command
+program.command('init <app-name>').action((appName) => {
+  console.log(chalk.yellow(`TODO: initAction in ${appName}`));
+});
+
+program.parse(process.argv);
