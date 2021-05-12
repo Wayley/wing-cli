dbd2583eb62094026f50f0cf147f53640486a7fb
wangzheng <zheng.wang@fih-foxconn.com>
2021-05-12 15:43:11 +0800

    removed the structure

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const { engines, name, version, copyright } = require('../package.json');
-
-// Configs of CLI
-module.exports = {
-  engines, // CLI Expected Node Engines
-  name, // CLI Name
-  version, // CLI Version
-  copyright: `Copyright © ${copyright} 2017-${new Date().getFullYear()}`, // CLI Copyright
-};
