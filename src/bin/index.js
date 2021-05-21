1644313ffc44ac720dbe155910b1ac30d829b875
wangzheng <zheng.wang@fih-foxconn.com>
2021-05-21 17:51:36 +0800

    modified the action

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,6 +1,46 @@
 #!/usr/bin/env node
 
 'use strict';
-const { copyright } = require('../config/index.js');
-console.log(123);
-console.log({ copyright });
+
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+const { satisfies } = require('semver');
+const chalk = require('chalk');
+const { name, version, engines } = require('../config');
+function checkNodeVersion(expectedVersion, packageName) {
+  if (!satisfies(process.version, expectedVersion)) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}, but this version of ${packageName} requires Node ${expectedVersion}'.\n\nPlease upgrade your Node version.`
+      )
+    );
+    process.exit(1);
+  }
+}
+checkNodeVersion(engines.node, name);
+
+// Add Commands
+const program = require('commander');
+const {
+  createAction,
+  initAction,
+  pluginAction,
+} = require('../lib/action/index.js');
+program.version(version).usage('<command> [options]');
+
+// `create` Command
+program.command('create <app-name>').action((appName) => {
+  createAction(appName);
+});
+
+// `init` Command
+program.command('init <app-name>').action((appName) => {
+  initAction(appName);
+});
+
+// `plugin` Command
+program.command('plugin <plugin-name>').action((pluginName) => {
+  pluginAction(pluginName);
+});
+
+program.parse(process.argv);
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
 const name = require('../../package.json').name;
 const version = require('../../package.json').version;
 const copyright = require('../../package.json').copyright;
+const engines = require('../../package.json').engines;
 module.exports = {
-  copyright: `${name} V${version} Copyright © ${copyright} 2017-${new Date().getFullYear()}`, // CLI Copyright
+  name,
+  version,
+  engines,
+  copyright: `Copyright © ${copyright} 2017-${new Date().getFullYear()}`, // CLI Copyright
 };
