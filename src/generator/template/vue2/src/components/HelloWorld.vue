d10a0ff57584fa3bd159b5fa3dd8cab2c7826b57
wangzheng <zheng.wang@fih-foxconn.com>
2021-12-09 16:25:39 +0800

    add vue2 template

---
+++ src/generator/template/vue2/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  color: #777;
+}
+</style>
