69522690dcbc4a9efa3597232dce1494da7d9387
wangzheng <zheng.wang@fih-foxconn.com>
2021-12-09 15:32:40 +0800

    modified

--- src/lib/Creator.js
+++ src/lib/Creator.js
@@ -1,3 +1,5 @@
+const { writeFiles } = require('../util');
+
 function Creator(options) {
   for (const key in options) {
     if (Object.hasOwnProperty.call(options, key)) {
@@ -5,5 +7,13 @@ function Creator(options) {
     }
   }
 }
-Creator.prototype.create = async function () {};
+Creator.prototype.create = async function () {
+  const { name, targetDir } = this;
+  const dateTime = new Date().getTime();
+  const package = { name, version: '0.0.1x', dateTime };
+  await writeFiles(targetDir, {
+    'package.json': JSON.stringify(package, null, 2),
+  });
+  console.log('Creator.create', dateTime);
+};
 module.exports = Creator;
