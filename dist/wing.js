a267d9292feefefded948669d63a4b6ad5fa9bf7
wangzheng <zheng.wang@fih-foxconn.com>
2021-12-10 14:29:09 +0800

    add leven

--- dist/wing.js
+++ dist/wing.js
@@ -5,4 +5,74 @@
  * Copyright © 2017-2021 Robben Wadlay
  * Released under the MIT License
  *
- */(()=>{var e={306:e=>{"use strict";e.exports=JSON.parse('{"u2":"wingjs-cli","i8":"0.0.11","h$":{"node":"^10.12.0 || ^12.0.0 || >= 14.0.0"}}')},941:e=>{e.exports=e=>{console.log(`You made ${e} with 'create' command`)}},116:(e,o,r)=>{const s=r(941),t=r(623),i=r(297);e.exports={createAction:s,initAction:t,pluginAction:i}},623:e=>{e.exports=e=>{console.log(`You made ${e} with 'init' command`)}},297:e=>{e.exports=e=>{console.log(`You made ${e} with 'plugin' command`)}},242:e=>{"use strict";e.exports=require("chalk")},647:e=>{"use strict";e.exports=require("commander")},513:e=>{"use strict";e.exports=require("semver")}},o={};function r(s){var t=o[s];if(void 0!==t)return t.exports;var i=o[s]={exports:{}};return e[s](i,i.exports,r),i.exports}(()=>{"use strict";const{satisfies:e}=r(513),o=r(242),s=r(306).u2,t=r(306).i8,i=r(306).h$;var n,c;n=i.node,c=s,e(process.version,n)||(console.log(o.red(`You are using Node ${process.version}, but this version of ${c} requires Node ${n}'.\n\nPlease upgrade your Node version.`)),process.exit(1));const a=r(647),{createAction:p,initAction:u,pluginAction:m}=r(116);a.version(t).usage("<command> [options]"),a.command("create <app-name>").action((e=>{p(e)})),a.command("init <app-name>").action((e=>{u(e)})),a.command("plugin <plugin-name>").action((e=>{m(e)})),a.parse(process.argv)})()})();
+ */ (() => {
+  'use strict';
+  var e = {
+      306: (e) => {
+        e.exports = JSON.parse(
+          '{"u2":"wingjs-cli","i8":"0.0.11","h$":{"d":"^10.12.0 || ^12.0.0 || >= 14.0.0"}}'
+        );
+      },
+      242: (e) => {
+        e.exports = require('chalk');
+      },
+      647: (e) => {
+        e.exports = require('commander');
+      },
+      513: (e) => {
+        e.exports = require('semver');
+      },
+    },
+    o = {};
+  function r(s) {
+    var n = o[s];
+    if (void 0 !== n) return n.exports;
+    var t = (o[s] = { exports: {} });
+    return e[s](t, t.exports, r), t.exports;
+  }
+  (() => {
+    const e = r(513).satisfies,
+      o = r(242);
+    const s = r(306).u2;
+    var n, t;
+    (n = r(306).h$.d),
+      (t = s),
+      e(process.version, n) ||
+        (console.log(
+          o.red(
+            `You are using Node ${process.version}, but this version of ${t} requires Node ${n}'.\n\nPlease upgrade your Node version.`
+          )
+        ),
+        process.exit(1));
+    const i = ['8.x', '9.x', '11.x', '13.x'];
+    for (const r of i)
+      e(process.version, r) &&
+        console.log(
+          o.red(
+            `You are using Node ${process.version}.\nNode.js ${r} has already reached end-of-life and will not be supported in future major releases.\nIt's strongly recommended to use an active LTS version instead.`
+          )
+        );
+    const a = r(647);
+    a.version(`${r(306).i8}`).usage('<command> [options]'),
+      a
+        .command('create <app-name>')
+        .description(`create a new project powered by ${s} service`)
+        .option('-p, --port [presetName]', 'locale running port', 3e3)
+        .action((e, o) => {
+          const r = (function (e) {
+            const o = {};
+            return (
+              e.options.forEach((r) => {
+                const s = r.long
+                  .replace(/^--/, '')
+                  .replace(/-(\w)/g, (e, o) => (o ? o.toUpperCase() : ''));
+                'function' != typeof e[s] && void 0 !== e[s] && (o[s] = e[s]);
+              }),
+              o
+            );
+          })(o);
+          console.log('Create33', e, r);
+        }),
+      a.parse(process.argv);
+  })();
+})();
--- src/bin/wing.js
+++ src/bin/wing.js
@@ -2,4 +2,151 @@
 
 'use strict';
 
-console.log('Wing Welcome you');
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+
+const satisfies = require('semver').satisfies;
+const chalk = require('chalk');
+const packageName = require('../../package.json').name;
+function checkNodeVersion(expectedVersion, packageName) {
+  if (!satisfies(process.version, expectedVersion)) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}, but this version of ${packageName} requires Node ${expectedVersion}'.\n\nPlease upgrade your Node version.`
+      )
+    );
+    process.exit(1);
+  }
+}
+checkNodeVersion(require('../../package.json').engines.node, packageName);
+
+const EOL_NODE_MAJORS = ['8.x', '9.x', '11.x', '13.x'];
+for (const major of EOL_NODE_MAJORS) {
+  if (satisfies(process.version, major)) {
+    console.log(
+      chalk.red(
+        `You are using Node ${process.version}.\n` +
+          `Node.js ${major} has already reached end-of-life and will not be supported in future major releases.\n` +
+          `It's strongly recommended to use an active LTS version instead.`
+      )
+    );
+  }
+}
+
+const program = require('commander');
+const leven = require('leven');
+function getCliName() {
+  const path = require('path');
+  let _NAME_;
+  if (process.argv && process.argv[1]) {
+    _NAME_ = path.basename(process.argv[1], path.extname(process.argv[1]));
+  }
+  return _NAME_;
+}
+const _CLI_NAME_ = getCliName() || packageName;
+
+program
+  .version(`${require('../../package.json').version}`)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description(`create a new project powered by ${_CLI_NAME_} service`)
+  .option('-p, --port [port]', 'locale running port', 3000)
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd);
+    console.log('Create', name, options);
+  });
+const SUPPORT_UI_THEMES = [
+  { label: 'OTA Style', value: 'ota' },
+  { label: 'PSS Style', value: 'pss' },
+  { label: 'Dashboard Style', value: 'dashboard' },
+];
+const SUPPORT_FRAMEWORKS = [
+  { label: 'Vue2.x', value: 'vue2' },
+  { label: 'Vue3.x', value: 'vue3' },
+  { label: 'React16.x', value: 'react16' },
+];
+function renderDisplay(list) {
+  return list.map((o) => '`' + o['label'] + '(' + o['value'] + ')`').join(', ');
+}
+program
+  .command('init <app-name>')
+  .description(`quickly init a new project powered by ${_CLI_NAME_} service`)
+  .option(
+    '-f, --framework [framework]',
+    `javascript framework, incluing ${renderDisplay(SUPPORT_FRAMEWORKS)}`,
+    'vue2'
+  )
+  .option(
+    '-u, --ui-theme [uiTheme]',
+    `we provide some ui themes, incluing ${renderDisplay(SUPPORT_UI_THEMES)}`,
+    'ota'
+  )
+  .option('-p, --port [port]', 'locale running port', 3000)
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd);
+    console.log('init', name, options);
+  });
+
+// output help information on unknown commands
+program.arguments('[command]').action((cmd) => {
+  program.outputHelp();
+  if (cmd) {
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+    console.log();
+    suggestCommands(cmd);
+  }
+});
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log();
+  console.log(
+    `  Run ${chalk.cyan(
+      `${_CLI_NAME_} <command> --help`
+    )} for detailed usage of given command.`
+  );
+  console.log();
+});
+
+program.commands.forEach((c) => c.on('--help', () => console.log()));
+
+program.parse(process.argv);
+
+function suggestCommands(unknownCommand) {
+  const availableCommands = program.commands.map((cmd) => cmd._name);
+
+  let suggestion;
+
+  availableCommands.forEach((cmd) => {
+    const isBestMatch =
+      leven(cmd, unknownCommand) < leven(suggestion || '', unknownCommand);
+    if (leven(cmd, unknownCommand) < 3 && isBestMatch) {
+      suggestion = cmd;
+    }
+  });
+
+  if (suggestion) {
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`));
+  }
+}
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs(cmd) {
+  const args = {};
+  cmd.options.forEach((o) => {
+    const key = camelize(o.long.replace(/^--/, ''));
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key];
+    }
+  });
+  return args;
+}
